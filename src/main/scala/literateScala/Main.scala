d4d62e03ea5440e626833e95b6fa476cf1dc1da8
Kim Stebel <mail@kim-stebel.com>
2012-12-17 22:21:47 +0100

    first working version, yay

--- src/main/scala/literateScala/Main.scala
+++ src/main/scala/literateScala/Main.scala
@@ -1,26 +1,55 @@
 package literateScala
 
 import java.io.File
+import org.apache.commons.lang3.StringEscapeUtils
+import java.io.BufferedWriter
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 
 object Main {
 
   def main(args:Array[String]) {
-    val files = List(
-        "/src/main/scala/sprouch/tutorial/comment",
-        "/build.sbt"
-    ).map("/home/k/workspaces/sprouch-tutorial/" +).map(new File(_)) //args.toList
-      val commentRegexStr = """(?s)(?m)/\*(.*?)\*/|//(.*?)$"""
-      val commentRegex = commentRegexStr.r
-      val contents = files.map(f => {
-      val source = scala.io.Source.fromFile("Main.scala")
-      val str = source.getLines.mkString
-      def escape(s:String) = StringEscapeUtils.escapeHtml4(s);
-      val textBlocks = commentRegex.findAllIn(str).toList.map("<p>" + _ + "</p>")
-      val codeBlocks = str.split(commentRegexStr).toList.map("<pre>" + _ + "</pre>")
-
-      
+    val files = args.toList.map(new File(_))
+    val commentRegexStr = """(?s)(?m)/\*(.*?)\*/|//(.*?)\n"""
+    val commentRegex = commentRegexStr.r
+    val contents = files.map(f => {
+      val source = scala.io.Source.fromFile(f)
+      val str = source.getLines.mkString("\n")
+      def escape(s:String) = StringEscapeUtils.escapeHtml4(s)
+      def cleanText(t:String) = {
+        val text = t.trim
+        if (text.startsWith("//")) {
+          text.substring(2)
+        } else {
+          if (text.startsWith("/*")) {
+            text.substring(2, text.length - 2)
+          } else {
+            throw new Exception("text is neither // nor /* style comment")
+          }
+        }.trim
+      }
+      def cleanCode(c:String) = {
+        val cleaner = """(?s)(?m)(.*?)\s*""".r
+        c match {
+          case cleaner(res) => res
+          case _ => throw new Exception("'" + c + "' did not match")
+        }
+      }
+      val textBlocks = commentRegex.findAllIn(str).toList.map(cleanText).map(text => "<p>" + text + "</p>")
+      val codeBlocks = str.split(commentRegexStr).map(cleanCode).filter("" !=).map { code => 
+        "<pre>" + escape(code) + "</pre>"
+      }
+      f -> textBlocks.zipAll(codeBlocks, "", "").map { case (t,c) => t + c }.mkString
     })
-    
+    val htmlDocs = contents.map { case (f,c) => 
+      f -> ("<html><head><title>literate scala</title></head><body>" + c + "</body></html>")
+    }
+    htmlDocs.foreach { case (file,htmlDoc) => {
+      val outFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(file.getName + ".html"))))
+      outFile.write(htmlDoc)
+      outFile.close()
+     
+    }}
   }
 
 }
